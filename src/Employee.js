3b3a343ff4cbf8f3e8f5d2eef0232efe2f9a2843
rkkmkkfx <baranovs-v@mail.ru>
2017-10-02 18:18:58 +0800

    add npm-run-all package

--- src/Employee.js
+++ src/Employee.js
@@ -8,11 +8,6 @@ class Employee extends Component {
   render() {
     const { id, firstName, lastName, departmentId } = this.props.data;
     const panelFooter = <EmployeeModal data={ this.props.data } update={this.update.bind(this, id)}/>;
-    let departmentName;
-    this.getDepartmentsName(departmentId).
-      then(name => {
-
-    });
     return (
       <Col xs={ 12 } md={ 4 }>
         <Panel footer={ panelFooter }>
@@ -26,14 +21,6 @@ class Employee extends Component {
 
   update(id) {
     this.props.update(id);
-  }
-
-  getDepartmentsName(id) {
-    return fetch('/departments/' + id, {accept: 'application/json'})
-      .then(res => res.json())
-      .then(item => {
-        return item.name;
-      });
   }
 }
 
